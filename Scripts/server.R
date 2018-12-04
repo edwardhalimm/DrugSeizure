b68f32a428d1641264827dc7da5afbe150cbd0cb
sw.stanleywu@gmail.com <sw.stanleywu@gmail.com>
2018-12-04 11:24:55 -0800

    Relationship between countries

--- Scripts/server.R
+++ Scripts/server.R
@@ -20,14 +20,57 @@ library(googleVis)
 
 # token <- pk.eyJ1IjoiamFuZXR0ZWN3ayIsImEiOiJjanA2ZHJwcW0wOHk3M3BvNmNlYWE2dGJ5In0.ZsZjug12tYHP1K_751NFWA
 #maptile <- "https://api.mapbox.com/v4/mapbox.emerald/page.html?access_token=pk.eyJ1IjoiamFuZXR0ZWN3ayIsImEiOiJjanA2ZHJwcW0wOHk3M3BvNmNlYWE2dGJ5In0.ZsZjug12tYHP1K_751NFWA"
+
+
 shinyServer(function(input, output) {
   output$seizures_map <- renderLeaflet({
     leaflet() %>% 
       addTiles() 
   })
+  
+  
   output$relationship_map <- renderLeaflet({
-    leaflet() %>%
-      addTiles()
+    setwd('c:/Users/Stanley/Desktop/Info201/Final/final_project_info_201/')
+    drug_data <- read_xlsx("data/IDSReport.xlsx", sheet = 6, col_names = TRUE)
+    location_data <- read_xlsx("data/Location_longitude_latitude.xlsx", col_names = TRUE)
+    val <- 0
+    df <- data.frame(lat=numeric(0), lng=numeric(0), stringsAsFactors=FALSE) 
+    selected_country <- filter(drug_data, input$country2 == COUNTRY)
+    as.data.frame(selected_country)
+    
+    coordinates <- filter(location_data, input$country2 == name)
+    select_lat <- coordinates$latitude[1]
+    select_lng <- coordinates$longitude[1]
+    
+    
+    for(row in 1:nrow(selected_country)) {
+      if(!is.na(selected_country$PRODUCING_COUNTRY[row]) & selected_country$PRODUCING_COUNTRY[row] != "Unknown") {
+        coordinates <- filter(location_data, selected_country$PRODUCING_COUNTRY[row] == name)
+        df[nrow(df)+1,] <- c(coordinates$latitude[1], coordinates$longitude[1])
+        if (input$country2== selected_country$PRODUCING_COUNTRY[row]) {
+          val <- 1 
+        }
+      }
+    }
+    
+    icon <- awesomeIcons(icon = 'flag', iconColor = 'red')
+      if(val == 0) {
+        na.omit(df)
+        df %>%
+        leaflet() %>%
+        addTiles() %>%
+        addMarkers(popup="Producing Country") %>%
+        addAwesomeMarkers(lat = select_lat, lng = select_lng, icon = icon, popup="Seizure Country")
+      } else {
+          df %>%
+          leaflet() %>%
+          addTiles() %>%
+          addMarkers(popup="Producing Country") %>%
+          addAwesomeMarkers(lat = select_lat, lng = select_lng, icon = icon, popup="Seizure and Producing Country")
+      }
+    
+    
+     
   })
   
 })
