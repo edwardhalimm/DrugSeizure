adebb0c4079781d97a5571f24ba231bc6a0d81a7
Wai Kwan Janette Chow <chowwk@uw.edu>
2018-12-03 16:14:45 -0800

    Widget changed

--- Scripts/server.R
+++ Scripts/server.R
@@ -14,13 +14,14 @@ library(shiny)
 library(shinydashboard)
 library(graphics)
 library(googleVis)
-#data <- read_xlsx("../data/IDSReport.xlsx", sheet = 6)
-#data <- select(data, SUBREGION , COUNTRY, SEIZURE_DATE, DRUG_NAME, AMOUNT, DRUG_UNIT, 
-#                      DEPARTURE_COUNTRY, DESTINATION_COUNTRY)
-
+data <- suppressWarnings(read_xlsx("../data/IDSReport.xlsx", sheet = 6, col_names = TRUE))
+data <- select(data, SUBREGION , COUNTRY, SEIZURE_DATE, DRUG_NAME, AMOUNT, DRUG_UNIT, 
+                     DEPARTURE_COUNTRY, DESTINATION_COUNTRY)
 # token <- pk.eyJ1IjoiamFuZXR0ZWN3ayIsImEiOiJjanA2ZHJwcW0wOHk3M3BvNmNlYWE2dGJ5In0.ZsZjug12tYHP1K_751NFWA
 #maptile <- "https://api.mapbox.com/v4/mapbox.emerald/page.html?access_token=pk.eyJ1IjoiamFuZXR0ZWN3ayIsImEiOiJjanA2ZHJwcW0wOHk3M3BvNmNlYWE2dGJ5In0.ZsZjug12tYHP1K_751NFWA"
 shinyServer(function(input, output) {
+
+
   output$seizures_map <- renderLeaflet({
     leaflet() %>% 
       addTiles() 
